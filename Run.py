5cb27fe704ad4ee718441f491928b9abda8186e8
Stephen Hunter <retnuh1337@gmail.com>
2018-11-07 20:26:19 -0500

    Initial Commit

---
+++ Run.py
@@ -0,0 +1,25 @@
+import string
+from Read import getUser, getMessage
+from Socket import openSocket, sendMessage
+from Initialize import joinRoom
+
+s = openSocket()
+joinRoom(s)
+readbuffer = ""
+
+while True:
+    readbuffer = readbuffer + s.recv(1024)
+    temp = string.split(readbuffer, "\n")
+    readbuffer = temp.pop()
+		
+    for line in temp:
+        print(line)
+	if "PING" in line:
+	    s.send(line.replace("PING", "PONG"))
+	    break
+	user = getUser(line)
+	message = getMessage(line)
+	print (user + " typed :" + message)
+	if "You Suck" in message:
+	    sendMessage(s, "No, you suck!")
+	    break
---
+++ Settings.py
@@ -0,0 +1,6 @@
+HOST = "irc.twitch.tv"
+PORT = 6667
+PASS = ""
+IDENT = "caBot"
+CHANNEL = "centennialarts"
+
