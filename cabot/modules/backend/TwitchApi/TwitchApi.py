8b126244e227a992170c628aaf78ed275361f180
fjdrummond <fjdrummond@gmail.com>
2019-04-07 16:48:37 -0500

    Update TwitchApi.py
    WIP: POC works but needs

--- cabot/modules/backend/TwitchApi/TwitchApi.py
+++ cabot/modules/backend/TwitchApi/TwitchApi.py
@@ -7,6 +7,9 @@ import init
 
 # import global modules
 import requests
+import socket
+
+conn1 = None # TODO: Extract this out along with the `disco` command below
 
 class TwitchApi():
     def __init__(self, username, client_id, token, channel):
@@ -48,6 +51,11 @@ class TwitchApi():
         return "This is an example bot, replace this text with your raffle text."
     def schedule(self, message):
         return "This is an example bot, replace this text with your schedule text."
+    def disco(self, message): # TODO: Extract this and put it into its own socket external service listening thread
+        conn1.send('partyTime'.encode('utf-8')) # This is the hardcoded command to toggle the discoball light
+        #conn1.close()
+        return "Hopefully this worked!"
+    
 
 # initalize main module
 twitch_conf = init.config['TWITCHAPI']
@@ -64,3 +72,16 @@ init.addCommandAction('game',bot,'game')
 init.addCommandAction('title',bot,'title')
 init.addCommandAction('raffle',bot,'raffle')
 init.addCommandAction('schedule',bot,'schedule')
+init.addCommandAction('disco', bot, 'disco')
+
+# TODO: Put this into its own thread so it doesn't lock the main thread
+sock = socket.socket()
+host = '192.168.x.x' # Set this to your server's IP address
+port = 12345           # This port must be available and match what the external discoball service uses
+sock.bind((host,port))
+sock.listen(5)
+
+conn, addr = sock.accept()
+print('Got connection from', addr)
+conn.send('Thank you for connecting'.encode('utf-8'))
+conn1 = conn
