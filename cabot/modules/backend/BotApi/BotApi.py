b9c1db86231f147f381f11c5ddce57b1d5c8e6de
Joseph Hanna <josephhanna@centennialarts.com>
2019-05-11 13:37:16 -0500

    Creating the Folder and File Structure to Support Generic API and Bot Chaining

---
+++ cabot/modules/backend/BotApi/BotApi.py
@@ -0,0 +1,16 @@
+#
+# BotApi Action Module
+#
+
+# import config
+import init
+
+# import global modules
+import requests
+
+class BotApi():
+    def __init__(self):
+        return
+
+# initalize main module
+botApi = BotApi()
